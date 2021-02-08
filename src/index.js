43f770011e533858ca9842da148ffde667aa5ec1
Chris Johnson <crjjrc@gmail.com>
2021-02-07 19:32:20 -0500

    Separate cube faces.

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function render() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.viewport(0, 0, canvas.width, canvas.height);
 
+  gl.enable(gl.DEPTH_TEST);
+
   shader.bind();
   vertexArray.bind();
   shader.setUniformMatrix4("eyeToClip", eyeToClip);
@@ -32,9 +34,9 @@ function onSizeChanged() {
   const aspect = canvas.width / canvas.height;
   const size = 2;
   if (aspect >= 1) {
-    eyeToClip = Matrix4.ortho(-size * aspect, size * aspect, -size, size, 10, -10);
+    eyeToClip = Matrix4.ortho(-size * aspect, size * aspect, -size, size, -10, 10);
   } else {
-    eyeToClip = Matrix4.ortho(-size, size, -size / aspect, size / aspect, 10, -10);
+    eyeToClip = Matrix4.ortho(-size, size, -size / aspect, size / aspect, -10, 10);
   }
   trackball.setViewport(canvas.width, canvas.height);
   render();
@@ -42,33 +44,97 @@ function onSizeChanged() {
 
 function makeBox() {
   const positions = [
+    // Front
     -1, -1, 1,
-    1, -1, 1,
-    -1, 1, 1,
-    1, 1, 1,
+     1, -1, 1,
+    -1,  1, 1,
+     1,  1, 1,
+
+     // Top
+    -1,  1, 1,
+     1,  1, 1,
+    -1,  1, -1,
+     1,  1, -1,
+
+     // Back
+     1, -1, -1,
     -1, -1, -1,
-    1, -1, -1,
-    -1, 1, -1,
-    1, 1, -1,
+     1,  1, -1,
+    -1,  1, -1,
+
+    // Bottom
+    -1, -1, -1,
+     1, -1, -1,
+    -1, -1,  1,
+     1, -1,  1,
+
+    // Right
+     1,  1, -1,
+     1, -1, -1,
+     1,  1,  1,
+     1, -1,  1,
+
+     //Left
+     -1,  1, -1,
+     -1, -1, -1,
+     -1,  1,  1,
+     -1, -1,  1,
+
+  ];
+
+  const normals = [
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+    0, 0, -1,
+    0, 0, -1,
+    0, 0, -1,
+    0, 0, -1,
+    0, -1, 0,
+    0, -1, 0,
+    0, -1, 0,
+    0, -1, 0,
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+    -1, 0, 0,
+
+
+    
   ];
 
   const faces = [
     0, 1, 2,
     1, 3, 2,
-    4, 7, 5,
-    4, 6, 7,
-    1, 5, 3,
-    3, 5, 7,
-    0, 6, 4,
-    2, 6, 0,
-    2, 3, 7,
-    2, 7, 6,
-    0, 7, 1,
-    0, 6, 7,
+
+    4, 5, 6,
+    5, 7, 6,
+
+    8, 9, 10,
+    9, 11, 10,
+
+    12, 13, 14,
+    13, 15, 14,
+
+    16, 17, 18,
+    17, 19, 18,
+    
+    20, 21, 22,
+    21, 23, 22,
   ];
 
   const attributes = new VertexAttributes();
-  attributes.addAttribute("position", 8, 3, positions);
+  attributes.addAttribute("position", 24, 3, positions);
+  attributes.addAttribute("normal", 24, 3, normals);
   attributes.addIndices(faces);
 
   vertexArray = new VertexArray(shader, attributes);
@@ -81,17 +147,27 @@ async function initialize() {
 uniform mat4 eyeToClip;
 uniform mat4 rotation;
 in vec3 position;
+in vec3 normal;
+
+out vec3 fnormal;
 
 void main() {
   gl_Position = eyeToClip * rotation * vec4(position, 1.0);
+  fnormal = (rotation * vec4(normal, 0.0)).xyz;
 }
   `;
 
   const fragmentSource = `
+in vec3 fnormal;
 out vec4 fragmentColor;
 
+const vec3 light_direction = normalize(vec3(1.0, 1.0, 1.0));
+
 void main() {
+  vec3 normal = normalize(fnormal);
+  float litness = max(0.0, dot(normal, light_direction));
   fragmentColor = vec4(0.0, 1.0, 0.6, 1.0);
+  fragmentColor = vec4(vec3(litness), 1.0);
 }
   `;
   shader = new ShaderProgram(vertexSource, fragmentSource);
